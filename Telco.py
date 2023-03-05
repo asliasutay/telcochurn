89cb692143b6175552fb7a44a7d08b2c8f088ed2
Aslı <75916636+asliasutay@users.noreply.github.com>
2023-03-05 23:57:14 +0300

    Add files via upload

---
+++ Telco.py
@@ -0,0 +1,193 @@
+#Görev 1 : Keşifçi Veri Analizi
+#Adım 1: Genel resmi inceleyiniz.
+
+import numpy as np
+import pandas as pd
+import missingno as msno
+from matplotlib import pyplot as plt
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+
+pd.set_option('display.max_columns', None)
+# pd.set_option('display.max_rows', None)
+pd.set_option('display.float_format', lambda x: '%.3f' % x)
+pd.set_option('display.width', 500)
+
+def load():
+    data = pd.read_csv("/Users/Asli1/datasets/Telco-Customer-Churn.csv")
+    return data
+
+
+df = load()
+
+df.describe().T
+df.info()
+
+df["TotalCharges"] = pd.to_numeric(df["TotalCharges"], errors="coerce") #numerik hale getirdik
+
+df.isnull()
+df.isnull().sum()
+
+
+#Adım 2: Numerik ve kategorik değişkenleri yakalayınız.
+
+def grab_col_names(dataframe, cat_th=10, car_th=20):
+    # cat_cols, cat_but_car
+    cat_cols = [col for col in dataframe.columns if dataframe[col].dtypes == "O"]
+    num_but_cat = [col for col in dataframe.columns if dataframe[col].nunique() < cat_th and
+                   dataframe[col].dtypes != "O"]
+    cat_but_car = [col for col in dataframe.columns if dataframe[col].nunique() > car_th and
+                   dataframe[col].dtypes == "O"]
+    cat_cols = cat_cols + num_but_cat
+    cat_cols = [col for col in cat_cols if col not in cat_but_car]
+
+    # num_cols
+    num_cols = [col for col in dataframe.columns if dataframe[col].dtypes != "O"]
+    num_cols = [col for col in num_cols if col not in num_but_cat]
+
+    print(f"Observations: {dataframe.shape[0]}")
+    print(f"Variables: {dataframe.shape[1]}")
+    print(f'cat_cols: {len(cat_cols)}')
+    print(f'num_cols: {len(num_cols)}')
+    print(f'cat_but_car: {len(cat_but_car)}')
+    print(f'num_but_cat: {len(num_but_cat)}')
+    return cat_cols, num_cols, cat_but_car
+
+grab_col_names(df)
+
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+
+##Adım 3: Numerik ve kategorik değişkenlerin analizini yapınız.
+
+#elimizde 3 numerik değişken var. ayrıca 17 tane kategorik değişken var.
+#customerID kardinal değişkenimiz.
+
+num_cols
+cat_cols
+
+##Adım 4: Hedef değişken analizi yapınız.(Kategorik değişkenlere göre
+# hedef değişkenin ortalaması, hedef değişkene göre numerik değişkenlerin ortalaması)
+
+for col in num_cols:
+    print(pd.DataFrame(df.groupby("Churn")[col].mean()))
+
+#for col in cat_cols:
+#   print(pd.DataFrame(df.groupby(col)["Churn"].mean()))
+
+#Adım 5: Aykırı gözlem analizi yapınız.
+
+def outlier_thresholds(dataframe, col_name, q1=0.25, q3=0.75):
+    quartile1 = dataframe[col_name].quantile(q1)
+    quartile3 = dataframe[col_name].quantile(q3)
+    interquantile_range = quartile3 - quartile1
+    up_limit = quartile3 + 1.5 * interquantile_range
+    low_limit = quartile1 - 1.5 * interquantile_range
+    return low_limit, up_limit
+
+def check_outlier(dataframe, col_name):
+    low_limit, up_limit = outlier_thresholds(dataframe, col_name)
+    if dataframe[(dataframe[col_name] > up_limit) | (dataframe[col_name] < low_limit)].any(axis=None):
+        return True
+    else:
+        return False
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+    
+#Adım 6: Eksik gözlem analizi yapınız.
+
+def missing_values_table(dataframe, na_name=False):
+    na_columns = [col for col in dataframe.columns if dataframe[col].isnull().sum() > 0]
+
+    n_miss = dataframe[na_columns].isnull().sum().sort_values(ascending=False)
+    ratio = (dataframe[na_columns].isnull().sum() / dataframe.shape[0] * 100).sort_values(ascending=False)
+    missing_df = pd.concat([n_miss, np.round(ratio, 2)], axis=1, keys=['n_miss', 'ratio'])
+    print(missing_df, end="\n")
+
+    if na_name:
+        return na_columns
+
+missing_values_table(df)
+
+#Adım 7: Korelasyon analizi yapınız.
+
+#df.corr().sort_values("Churn", ascending=False)
+
+msno.bar(df)
+plt.show()
+
+msno.matrix(df)
+plt.show()
+
+msno.heatmap(df)
+plt.show()
+
+corr_matrix = df.corr()
+
+#Görev 2 : Feature Engineering
+#Adım 1: Eksik ve aykırı değerler için gerekli işlemleri yapınız.
+
+def replace_with_thresholds(dataframe, variable):
+    low_limit, up_limit = outlier_thresholds(dataframe, variable)
+    dataframe.loc[(dataframe[variable] < low_limit), variable] = low_limit
+    dataframe.loc[(dataframe[variable] > up_limit), variable] = up_limit
+
+for col in num_cols:
+    replace_with_thresholds(df, col)
+
+for col in num_cols:
+    print(col, check_outlier(df, col))
+
+df.isnull().values.any()
+
+#Adım 2: Yeni değişkenler oluşturunuz.
+
+df["HAS_INTERNET_SERVICE"] = df["InternetService"].apply(lambda x: "0" if x == "No" else "1")
+
+df["HAS_PHONE_SERVICE"] = df["PhoneService"].apply(lambda x: "0" if x == "No" else "1")
+
+df.loc[(df["HAS_INTERNET_SERVICE"] == "1") & (df["HAS_PHONE_SERVICE"] == "1"), "HAS_BOTH_SERVICES"] = "1"
+df["HAS_BOTH_SERVICES"].fillna("0", inplace=True)
+
+df['TOTALSERVICES'] = (df[['PhoneService', 'InternetService', 'OnlineSecurity', 'OnlineBackup', 'DeviceProtection',
+                               'TechSupport', 'StreamingTV', 'StreamingMovies']] == 'Yes').sum(axis=1)
+
+#Adım 3: Encoding işlemlerini gerçekleştiriniz.
+
+# LABEL ENCODING
+def label_encoder(dataframe, binary_col):
+    labelencoder = LabelEncoder()
+    dataframe[binary_col] = labelencoder.fit_transform(dataframe[binary_col])
+    return dataframe
+binary_cols = [col for col in df.columns if
+               df[col].dtype not in [int, float] and df[col].nunique() == 2]
+for col in binary_cols:
+    label_encoder(df, col)
+# ONE HOT ENCODING
+
+def one_hot_encoder(dataframe, categorical_cols, drop_first=True):
+    dataframe = pd.get_dummies(dataframe, columns=categorical_cols, drop_first=drop_first)
+    return dataframe
+cat_cols, num_cols, cat_but_car = grab_col_names(df)
+ohe_cols = [col for col in df.columns if 10 >= df[col].nunique() > 2]
+one_hot_encoder(df, ohe_cols)
+one_hot_encoder(df, cat_cols)
+
+#Adım 4: Numerik değişkenler için standartlaştırma yapınız.
+scaler = StandardScaler()
+df[num_cols] = scaler.fit_transform(df[num_cols])
+
+#Adım 5: Model oluşturunuz
+
+
+y = df["Churn"]
+X = df.drop(["customerID", "Churn"], axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.30, random_state=17)
+
+from sklearn.ensemble import RandomForestClassifier
+
+rf_model = RandomForestClassifier(random_state=46).fit(X_train, y_train)
+y_pred = rf_model.predict(X_test)
+accuracy_score(y_pred, y_test)
